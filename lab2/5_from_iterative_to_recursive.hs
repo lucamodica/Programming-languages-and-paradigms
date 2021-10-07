3e838202bb41655e212be35052d47d80612a6988
Luca <Modica>
2021-10-07 10:00:52 +0100

    Partial lab2 file 5

---
+++ lab2/5_from_iterative_to_recursive.hs
@@ -0,0 +1,19 @@
+-- ##From iterative ro recursive##
+
+--es1
+factorial :: Int -> Int
+factorial = aux 1
+    where
+        aux res 0 = res
+        aux res n = aux (res * n) (n - 1)
+
+--es2
+bits :: Int -> Int
+bits = aux 0
+    where
+        aux bits 0 = bits
+        aux bits n = aux (bits + mod n 2) (div n  2)
+
+--es3
+euclid :: Int -> Int
+euclid aux 
