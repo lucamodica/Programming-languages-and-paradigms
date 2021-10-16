69fe57a3a07691601ccaaa74df8559ae74355b70
Luca Modica <luca.modica@edu.unito.it>
2021-10-16 15:45:48 +0200

    Added es about list transforamtion (lab4, file 4)

--- lab4/3_list_transformations_and_quick_sort.hs
+++ lab4/3_list_transformations_and_quick_sort.hs
@@ -10,3 +10,25 @@ length . filter (>= 0) -- returns the number of positive numbers
 foldr (&&) True . map (>= 0) -- check if all the numbers are positive
 foldr (+) 0 . map (const 1) -- returns the size of the list
 foldl (\xs x -> x : xs) [] -- basically reverse the list
+
+
+-- es3
+concat :: [[a]] -> [a]
+concat = foldl (++) []
+
+any :: (a -> Bool) -> [a] -> Bool
+any p = foldr (||) False . map p
+
+all :: (a -> Bool) -> [a] -> Bool
+all p = foldr (&&) True . map p
+
+
+-- es4
+massimo :: Ord a => [a] -> a
+massimo (x : xs) = foldr max x xs
+
+occorrenze :: Eq a => a -> [a] -> Int
+occorrenze x = length . filter (== x)
+
+membro :: Eq a => a -> [a] -> Bool
+membro x = any (== x)
