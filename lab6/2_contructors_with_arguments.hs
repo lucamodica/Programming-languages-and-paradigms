37149aa692e5d585054ee382c10c1f1551799b37
Luca Modica <luca.modica@edu.unito.it>
2021-11-05 18:00:28 +0100

    Added init lab JVMM

--- lab6/2_contructors_with_arguments.hs
+++ lab6/2_contructors_with_arguments.hs
@@ -2,16 +2,15 @@
 
 -- es1
 data MaybeInt = Nothingg | Justt Int
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 headd :: [Int] -> MaybeInt
-headd []      = Nothingg
+headd [] = Nothingg
 headd (x : _) = Justt x
-
 
 -- es2
 data Numero = I Int | F Float
-    deriving (Eq, Show)
+  deriving (Eq, Show)
 
 somma :: Numero -> Numero -> Numero
 somma (I x) (I y) = I (x + y)
@@ -19,14 +18,12 @@ somma (I x) (F y) = F (fromIntegral x + y)
 somma (F x) (I y) = F (x + fromIntegral y)
 somma (F x) (F y) = F (x + y)
 
-
 -- es3
 sommatoria :: [Numero] -> Numero
 sommatoria = foldr somma (I 0)
 
-
 -- es4
 proprio :: [MaybeInt] -> [Int]
-proprio []              = []
+proprio [] = []
 proprio (Nothingg : xs) = proprio xs
-proprio (Justt x : xs)  = x : proprio xs
+proprio (Justt x : xs) = x : proprio xs
