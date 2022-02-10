3a80d24ab1c9c1bc3402ea113f9b8d478e76f77d
Luca Modica <luca.modica@edu.unito.it>
2022-02-10 16:16:37 +0100

    Added more random es

--- random/random_es.hs
+++ random/random_es.hs
@@ -1,4 +1,7 @@
 -- useful data structures
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use even" #-}
 data Tree a = Node a [Tree a]
 
 -- ## es1: list as input, list of couple with element and the
@@ -70,4 +73,99 @@ count xs = length $ filter aux (words xs)
   where
     aux = all (\x -> x `elem` ['A' .. 'Z'])
 
--- >>>
+-- ## es9: 2 strings as input, returs a boolean to
+-- tell if the second string is inside the first one
+-- (recursive)
+isSubstring :: String -> String -> Bool
+isSubstring [] [] = True
+isSubstring _ [] = True
+isSubstring [] _ = False
+isSubstring xs ys = aux xs ys 0 0
+  where
+    aux xs ys ix iy
+      | lengthh ys == iy = True
+      | lengthh xs == ix = False
+      | (xs !! ix) == (ys !! iy) = aux xs ys (ix + 1) (iy + 1)
+      | (xs !! ix) /= (ys !! iy) = aux xs ys (ix + 1) 0
+      | otherwise = False
+    lengthh [] = 0
+    lengthh (x : xs) = 1 + lengthh xs
+
+-- ## es10: given an integer n and a value v returns a
+-- list of length n initialized with v, namely all elements
+-- are equal to v ##
+-- Recursive
+myReplicateR :: Int -> a -> [a]
+myReplicateR 0 _ = []
+myReplicateR n v = v : myReplicateR (n - 1) v
+
+-- With prelude funcs
+myReplicateC :: Int -> a -> [a]
+myReplicateC = replicate
+
+-- ## es11 given a list of integers computes the sum of
+-- the values that are odd ##
+-- Recursive
+sumOddR :: [Int] -> Int
+sumOddR [] = 0
+sumOddR (x : xs)
+  | mod x 2 == 0 = sumOddR xs
+  | otherwise = x + sumOddR xs
+
+-- With prelude funcs
+sumOddC :: [Int] -> Int
+sumOddC = sum . filter odd
+
+-- ## es12: given a list xs and a integer n returns a list
+-- containing the elements that is replicated at least n times
+-- in xs ##
+-- Recursive
+replR :: Eq a => [a] -> Int -> [a]
+replR [] _ = []
+replR _ 0 = []
+replR xs n = auxAux xs n []
+  where
+    auxAux [] _ fs = fs
+    auxAux (x : xs) n fs
+      | not (isIn fs x) && aux (x : xs) (replAux x n) x n = auxAux xs n (x : fs)
+      | otherwise = auxAux xs n fs
+    aux [] [] _ _ = True
+    aux [] _ _ _ = False
+    aux _ [] _ _ = True
+    aux (x : xs) (r : rs) e n
+      | x == r = aux xs rs e n
+      | otherwise = aux xs (replAux e n) e n
+    isIn [] _ = False
+    isIn (x : xs) e
+      | x == e = True
+      | otherwise = isIn xs e
+    replAux _ 0 = []
+    replAux x n = x : replAux x (n - 1)
+
+-- With prelude funcs
+-- TO DO
+
+-- ## es13: given a list of strings xs returns the total
+-- number of vowels in strings that are palindromes ##
+-- Recursive
+countVowelPaliR :: [String] -> Int
+countVowelPaliR [] = 0
+countVowelPaliR (x : xs)
+  | x == reversee x = countVowel x + countVowelPaliR xs
+  | otherwise = countVowelPaliR xs
+  where
+    reversee [] = []
+    reversee (x : xs) = reversee xs ++ [x]
+    countVowel [] = 0
+    countVowel (x : xs)
+      | x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' = 1 + countVowel xs
+      | otherwise = countVowel xs
+
+-- With prelude funcs
+countVowelPaliC :: [String] -> Int
+countVowelPaliC xss = sum $ map (length . filterVowels) (filter (\xs -> xs == reverse xs) xss)
+  where
+    filterVowels = filter (\x -> x `elem` ['a', 'e', 'i', 'o', 'u'])
+
+-- >>> countVowelPaliC ["ciao", "come", "otto", "acaca"]
+-- 5
